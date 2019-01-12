b58fa70248718f7f906f33a860a0167227b8ba4a
Anders Brander <anders@brander.dk>
2019-01-12 20:50:27 +0100

    Added package documentation.

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package connect provides access to the unofficial Garmin Connect API. This
+// is not supported or endorsed by Garmin Ltd. The API may change or stop
+// working at any time. Please use responsible.
+package connect
