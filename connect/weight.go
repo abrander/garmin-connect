e44870777e5ffee7880afad528a9578588e2aea4
Anders Brander <anders@brander.dk>
2019-03-01 11:16:17 +0100

    Added weight range command.

--- connect/weight.go
+++ connect/weight.go
@@ -35,6 +35,13 @@ func init() {
 		Args: cobra.ExactArgs(1),
 	}
 	weightCmd.AddCommand(weightDateCmd)
+
+	weightRangeCmd := &cobra.Command{
+		Use:  "range [yyyy-mm-dd] [yyyy-mm-dd]",
+		Run:  weightRange,
+		Args: cobra.ExactArgs(2),
+	}
+	weightCmd.AddCommand(weightRangeCmd)
 }
 
 func weightLatest(_ *cobra.Command, _ []string) {
@@ -80,4 +87,46 @@ func weightDate(_ *cobra.Command, args []string) {
 	t.AddValue("Time", tim.String())
 	t.AddValueUnit("Weight", weight/1000.0, "kg")
 	t.Output(os.Stdout)
+}
+
+func weightRange(_ *cobra.Command, args []string) {
+	from, err := connect.ParseDate(args[0])
+	bail(err)
+
+	to, err := connect.ParseDate(args[1])
+	bail(err)
+
+	average, weightins, err := client.Weightins(from.Time(), to.Time())
+	bail(err)
+
+	t := NewTabular()
+
+	t.AddValueUnit("Weight", average.Weight/1000.0, "kg")
+	t.AddValueUnit("BMI", average.BMI, "kg/m2")
+	t.AddValueUnit("Fat", average.BodyFatPercentage, "%")
+	t.AddValueUnit("Water", average.BodyWater, "%")
+	t.AddValueUnit("Bone Mass", float64(average.BoneMass)/1000.0, "kg")
+	t.AddValueUnit("Muscle Mass", float64(average.MuscleMass)/1000.0, "kg")
+	fmt.Fprintf(os.Stdout, "        \033[1mAverage\033[0m\n")
+	t.Output(os.Stdout)
+
+	t2 := NewTable()
+	t2.AddHeader("Date", "Weight", "BMI", "Fat%", "Water%", "Bone Mass", "Muscle Mass")
+	for _, weightin := range weightins {
+		if weightin.Weight < 1.0 {
+			continue
+		}
+
+		t2.AddRow(
+			weightin.Date.String(),
+			fmt.Sprintf("%.1f", weightin.Weight/1000.0),
+			fmt.Sprintf("%.1f", weightin.BMI),
+			fmt.Sprintf("%.1f", weightin.BodyFatPercentage),
+			fmt.Sprintf("%.1f", weightin.BodyWater),
+			fmt.Sprintf("%.1f", float64(weightin.BoneMass)/1000.0),
+			fmt.Sprintf("%.1f", float64(weightin.MuscleMass)/1000.0),
+		)
+	}
+	fmt.Fprintf(os.Stdout, "\n")
+	t2.Output(os.Stdout)
 }
