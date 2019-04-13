b57124be088b9de9ee1b58ced64d9caec5cfdb9e
Anders Brander <anders@brander.dk>
2019-04-13 11:51:01 +0200

    Removed unused function.

--- connect/tools.go
+++ connect/tools.go
@@ -14,14 +14,6 @@ func formatDate(t time.Time) string {
 	return fmt.Sprintf("%04d-%02d-%02d", t.Year(), t.Month(), t.Day())
 }
 
-func formatTime(t time.Time) string {
-	if t == (time.Time{}) {
-		return "never"
-	}
-
-	return fmt.Sprintf("%02d:%02d", t.Hour(), t.Minute())
-}
-
 func stringer(value interface{}) string {
 	stringer, ok := value.(fmt.Stringer)
 	if ok {
