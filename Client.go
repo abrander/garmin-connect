d0bf70f8f112415612591afc376b6804da3fdf0c
Anders Brander <anders@brander.dk>
2019-10-04 16:58:24 +0200

    Updated SSO URL to what we're actually requesting after SSO auth.

--- Client.go
+++ Client.go
@@ -347,7 +347,7 @@ func (c *Client) authenticated() bool {
 func (c *Client) Authenticate() error {
 	// We cannot use Client.do() in this function, since this function can be
 	// called from do() upon session renewal.
-	URL := "https://sso.garmin.com/sso/signin?service=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&gauthHost=https%3A%2F%2Fsso.garmin.com%2Fsso"
+	URL := "https://sso.garmin.com/sso/signin?service=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F&gauthHost=https%3A%2F%2Fconnect.garmin.com%2Fmodern%2F"
 
 	if c.Email == "" || c.Password == "" {
 		return ErrNoCredentials
