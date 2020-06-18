65e8b5ffe13780d62f649583021ee32744d15ee1
Anders Brander <anders@brander.dk>
2020-06-18 22:25:34 +0200

    Added JSON support for get command.

--- connect/get.go
+++ connect/get.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"os"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	formatJSON bool
 )
 
 func init() {
@@ -13,12 +20,27 @@ func init() {
 		Run:   get,
 		Args:  cobra.ExactArgs(1),
 	}
+	getCmd.Flags().BoolVarP(&formatJSON, "json", "j", false, "Format output as indented JSON")
 	rootCmd.AddCommand(getCmd)
 }
 
 func get(_ *cobra.Command, args []string) {
 	url := args[0]
 
-	err := client.Download(url, os.Stdout)
-	bail(err)
+	if formatJSON {
+		raw := bytes.NewBuffer(nil)
+		buffer := bytes.NewBuffer(nil)
+
+		err := client.Download(url, raw)
+		bail(err)
+
+		err = json.Indent(buffer, raw.Bytes(), "", "  ")
+		bail(err)
+
+		_, err = io.Copy(os.Stdout, buffer)
+		bail(err)
+	} else {
+		err := client.Download(url, os.Stdout)
+		bail(err)
+	}
 }
